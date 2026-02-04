8e6bccabf04c917d818746e46cad29484b5898c2
Vaskar-Bhattacharjee <vaskarbhattacharjee03@gmail.com>
2026-02-04 20:39:44 +0600

    Add fix code for debug

--- components/features-tertiary/skeleton/second.tsx
+++ components/features-tertiary/skeleton/second.tsx
@@ -137,8 +137,8 @@ export const InsideCircle = ({ className }: { className?: string }) => {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
+      strokeWidth="2"
+      strokeLinecap="round"
       stroke-linejoin="round"
     >
       <path stroke="none" d="M0 0h24v24H0z" fill="none" />
